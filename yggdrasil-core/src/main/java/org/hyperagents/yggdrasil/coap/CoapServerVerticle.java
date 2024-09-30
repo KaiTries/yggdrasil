fe701cc9d9e4ce869dad4cab101e7539507087f7
Kai <112092835+KaiTries@users.noreply.github.com>
2024-09-30 12:57:20 +0200

    WIP coAP: - joining workspace works

--- yggdrasil-core/src/main/java/org/hyperagents/yggdrasil/coap/CoapServerVerticle.java
+++ yggdrasil-core/src/main/java/org/hyperagents/yggdrasil/coap/CoapServerVerticle.java
@@ -1,35 +1,32 @@
 package org.hyperagents.yggdrasil.coap;
 
-import static org.eclipse.californium.core.coap.CoAP.ResponseCode.CONTENT;
-import static org.eclipse.californium.core.coap.MediaTypeRegistry.APPLICATION_JSON;
-import static org.eclipse.californium.core.coap.MediaTypeRegistry.TEXT_PLAIN;
-
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
-import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.config.CoapConfig;
-import org.eclipse.californium.core.server.resources.CoapExchange;
-import org.eclipse.californium.core.server.resources.Resource;
 import org.eclipse.californium.elements.config.UdpConfig;
+import org.hyperagents.yggdrasil.utils.EnvironmentConfig;
 import org.hyperagents.yggdrasil.utils.HttpInterfaceConfig;
 
 /**
- * CoAP server verticle, manages routing.
+ * CoAP server verticle, manages lifetime of coap server.
  */
 public class CoapServerVerticle extends AbstractVerticle {
 
   private CoapServer server;
   private HttpInterfaceConfig httpConfig;
+  private EnvironmentConfig environmentConfig;
 
   @Override
   public void start(final Promise<Void> startPromise) {
     this.httpConfig = this.vertx.sharedData()
         .<String, HttpInterfaceConfig>getLocalMap("http-config")
         .get("default");
+    this.environmentConfig = this.vertx
+        .sharedData()
+        .<String, EnvironmentConfig>getLocalMap("environment-config")
+        .get("default");
     startCoAPServer();
-
     startPromise.complete();
   }
 
@@ -42,68 +39,8 @@ public class CoapServerVerticle extends AbstractVerticle {
   private void startCoAPServer() {
     CoapConfig.register();
     UdpConfig.register();
-    this.server = new CoapServer();
-    createRouter();
+    final CoapEntityHandler handler = new CoapEntityHandler(vertx, httpConfig, environmentConfig);
+    this.server = new YggdrasilCoAPServer(handler);
     this.server.start();
-  }
-
-  private void createRouter() {
-    final CoapEntityHandler handler = new CoapEntityHandler(vertx, httpConfig);
-    // workspaces directory as resource, needed explicitly to enable wildcards
-    CoapResource workspaces = new Workspaces(handler);
-    this.server.add(workspaces);
-
-
-
-  }
-
-  static class WorkspaceHandlerResource extends CoapResource {
-    CoapEntityHandler handler;
-
-    public WorkspaceHandlerResource(CoapEntityHandler handler, String name) {
-      super(name, true);
-      this.handler = handler;
-      // find out if workspace exists
-    }
-
-    @Override
-    public void handleGET(final CoapExchange exchange) {
-      // on first get find out if workspace exists
-      handler.handleGetEntity(this, exchange);
-    }
-  }
-
-  static class Workspaces extends CoapResource {
-    private final CoapEntityHandler handler;
-
-    public Workspaces(CoapEntityHandler handler) {
-      super("workspaces");
-      this.handler = handler;
-      getAttributes().setTitle("workspaces");
-      getAttributes().addContentType(TEXT_PLAIN);
-      getAttributes().addContentType(APPLICATION_JSON);
-    }
-
-    @Override
-    public void handleGET(final CoapExchange exchange) {
-      // get request to read out details
-      exchange.accept();
-      // start asynchronous processing, passing the exchange to a result callback
-      Response response = new Response(CONTENT);
-      response.setPayload("workspaces");
-      exchange.respond(response);
-    }
-
-    @Override
-    public Resource getChild(String name) {
-      final var children = getChildren();
-      final var match = children.stream().filter(child -> child.getName().equals(name)).findFirst();
-      if (match.isPresent()) {
-        return match.get();
-      }
-      WorkspaceHandlerResource newWorkspace = new WorkspaceHandlerResource(handler, name);
-      this.add(newWorkspace);
-      return newWorkspace;
-    }
   }
 }
